01c6b3d9fe09bda70aa3e442b684f3a4487e537d
Abdullah Alyabroude <abu-omar.852@hotmail.com>
2026-01-04 01:52:29 +0100

    Fix FaselHD: Update domain to faselhds.biz

--- FaselHDProvider/build.gradle.kts
+++ FaselHDProvider/build.gradle.kts
@@ -9,7 +9,7 @@ cloudstream {
 
     tvTypes = listOf( "TvSeries" , "Movie" , "Anime" , "AsianDrama" )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=faselhd.io&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=faselhds.biz&sz=%size%"
     
     description = "Watch Arabic movies and series from FaselHD"
 }
